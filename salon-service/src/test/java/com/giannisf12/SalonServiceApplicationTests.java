34829b281b26020c59fa58473b81cb213921b1f7
GiannisF12 <44063119+GiannisF12@users.noreply.github.com>
2025-04-10 21:05:06 +0300

    Initial commit

---
+++ salon-service/src/test/java/com/giannisf12/SalonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giannisf12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
